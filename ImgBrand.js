692eb358bae427e62ae80df8c568460878cd780e
ant094 <cahyono.ari80@gmail.com>
2020-04-25 06:16:23 +0700

    _First Create Header And Footer

---
+++ ImgBrand.js
@@ -0,0 +1,13 @@
+class ImgBrand extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `<div class=" flex-fill text-center">
+                <img src="img/Lets'cook3.png" alt="" style="height: 150px;">
+            </div>`;
+    }
+
+}
+customElements.define("img-brand", ImgBrand);
---
+++ SearchRecipe.js
@@ -0,0 +1,13 @@
+class SearchRecipe extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `<div class="offset-lg-3 col-lg-6">
+                  <input class="form-control" type="text" placeholder="Search Your Receipe...">
+        </div>`;
+    }
+
+}
+customElements.define("search-recipe", SearchRecipe);
